c8c72321ccb4e9d3d6141855d234c2837d4b764e
sunya <mugen.xyz@gmail.com>
2017-01-21 02:14:18 +0900

    Fix authorize page

--- js/authorize.js
+++ js/authorize.js
@@ -1,73 +1,48 @@
-import Vue from 'vue'
 import promise from 'es6-promise'
-promise.polyfill()
-import 'isomorphic-fetch'
-
-import mixin from './base'
 import { generateURL } from './generateURL'
 
+import mixin from './base'
+
+promise.polyfill()
+
 const template = `
-<div>
-  <div v-show="authorizationURL">
-    <button @click="open">PINを取得</button>
-    <form v-show="showForm" @submit.prevent="submit">
-      <div>
-        <label for="pin">PIN: </label>
-        <input type="text" v-model="pin" required maxlength="7" pattern="^\\d{7}$" id="pin" />
-      </div>
-      <div>
-        <input type="submit" value="トークンを取得" :disabled="disableButton" />
-      </div>
-      <p v-show="promise"><partial name="progress" /></p>
-    </form>
+<form @submit.prevent="submit">
+  <div>
+    <label for="pin">PIN: </label>
+    <input type="text" v-model="pin" required maxlength="7" pattern="^\\d{7}$" id="pin" style="width: auto">
+    <input type="submit" value="トークンを取得" :disabled="disableButton">
+    <progress-ring v-show="processing">
   </div>
-  <div v-else>
-    <p v-show="!error">
-      <partial name="progress" />
-    </p>
-    <p v-else>エラーが発生しました。<a v-link="'/input-key'">1.に戻って</a>Consumer Key, Consumer Secret, 認証タイプを確認してください。</p>
-  </div>
-</div>
+  <p v-show="error">
+    エラーが発生しました。<router-link to="/input-key">1.に戻って</a>Consumer Key, Consumer Secret, 認証タイプを確認してください。
+  </p>
+</form>
 `
 
-export default Vue.extend({
+export default {
   mixins: [mixin],
   template,
   data() {
     return {
-      showForm: false,
-      promise: null,
-      authorizationURL: null,
-      pin: null,
-      error: null
+      processing: false,
+      error: false,
+      pin: null
     }
-  },
-  created() {
-    if(!this.checkKey()) return
-    this.generateOAuthURL()
-      .then(url => {
-        if(this.type === 'callback') {
-          location.href = url
-        } else {
-          this.authorizationURL = url
-        }
-      })
-      .catch(err => this.error = err)
   },
   computed: {
     disableButton() {
       return !/^\d{7}$/.test(this.pin) || this.promise
     }
   },
+  mounted() {
+    this.processing = false
+    const wnd = window.open(this.authorizationURL, 'ttg', 'resizable=yes,scrollbars=yes')
+    generateURL(this.$store.state.consumerKey, this.$store.state.consumerSecret, this.$store.state.type)
+      .then(url => wnd.location.href = url)
+  },
   methods: {
-    generateOAuthURL() {
-      return generateURL(this.consumerKey, this.consumerSecret, this.type)
-    },
-    open() {
-      window.open(this.authorizationURL, 'ttg', 'resizable=yes,scrollbars=yes')
-      this.showForm = true
-    },
     submit() {
+      this.processing = true
       this.promise = fetch(`/?oauth_verifier=${this.pin}`, {
         credentials: 'include',
         headers: {
@@ -84,14 +59,18 @@ export default Vue.extend({
             accessToken,
             accessTokenSecret
           } = data
-          this.accessToken = accessToken
-          this.accessTokenSecret = accessTokenSecret
-          this.$router.go('/authorized')
-          this.promise = null
+          this.$store.commit('updateValue', {
+            name: 'accessToken',
+            value: accessToken
+          })
+          this.$store.commit('updateValue', {
+            name: 'accessTokenSecret',
+            value: accessTokenSecret
+          })
+          this.processing = false
+          this.$router.push('/authorized')
         })
         .catch(err => this.error = err)
-        .then(() => this.generateOAuthURL())
-
     }
   }
-})
+}
