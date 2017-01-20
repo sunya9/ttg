54d78bc5b0e86faee81e40f52e9f54099c22df0d
sunya <mugen.xyz@gmail.com>
2017-01-21 02:09:29 +0900

    Show error message

--- js/input-key.js
+++ js/input-key.js
@@ -3,18 +3,21 @@ import { generateURL } from './generateURL'
 
 const template = `
   <form @submit.prevent="go">
+    <label>Consumer Key</label>
     <div>
-      <label>Consumer Key<input type="text" :value="key" name="consumerKey" @input="updateValue"></label>
+      <input type="text" :value="key" name="consumerKey" @input="updateValue" size="60">
     </div>
+    <label>Consumer Secret</label>
     <div>
-      <label>Consumer Secret<input type="text" :value="secret" name="consumerSecret" @input="updateValue"></label>
+      <input type="text" :value="secret" name="consumerSecret" @input="updateValue" size="60">
     </div>
     <div>
       <label>認証タイプ</label>
       <input type="radio" value="pin" @change="updateValue" name="type" id="type-pin" v-model="type"><label class="label-inline" for="type-pin">PIN</label>
       <input type="radio" value="callback" @change="updateValue" name="type" id="type-callback"  v-model="type"><label class="label-inline" for="type-callback">Callback</label>
     </div>
-    <input type="submit" :value="buttonText" :disabled="!key || !secret || !type">
+    <input type="submit" value="認証する" :disabled="!key || !secret || !type">
+    <p v-if="error">{{error.message}}</p>
   </form>
 `
 
@@ -22,23 +25,23 @@ export default {
   template,
   data() {
     return {
-      promise: null
+      error: null
     }
   },
   computed: mapState({
-    buttonText: state => state.type === 'callback' ? '認証する' : '認証URL生成',
     key: state => state.consumerKey,
     secret: state => state.consumerSecret,
     type: state => state.type
   }),
   methods: {
     go() {
+      this.error = null
       if(this.type === 'callback') {
-        this.promise = generateURL(this.key, this.secret, this.type)
+        generateURL(this.key, this.secret, this.type)
           .then(url => location.href = url)
-          .catch(console.error.bind(console))
+          .catch(err => this.error = err)
       } else {
-        this.$router[this.type === 'callback' ? 'replace' : 'push']('/authorize')
+        this.$router.push('/authorize')
       }
     },
     updateValue(e) {
