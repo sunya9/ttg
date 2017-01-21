86845d2514e4efaa5058d74dd03c938ee1418e78
sunya <mugen.xyz@gmail.com>
2017-01-21 22:19:21 +0900

    Remove unused files and rename.

--- js/base.js
+++
@@ -1,6 +0,0 @@
-export default {
-  updated() {
-    const goTop = !this.$store.state.consumerKey || !this.$store.state.consumerSecret || !this.$store.state.type
-    if(goTop) this.$router.replace('/')
-  }
-}
