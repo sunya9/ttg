cbc33889abb5a5f7c19d2d226935dd2fab4462e5
sunya9 <mugen.xyz@gmail.com>
2017-03-19 18:47:37 +0900

    Fix router-link

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
       <a href="https://github.com/sunya9/ttg"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 1" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
       <div class="container">
         <header>
-          <h1><router-link to="/">TwiTokenGetter</a></h1>
+          <h1><router-link to="/">TwiTokenGetter</router-link></h1>
           <p>TwiTokenGetterはTwitterのアクセストークン類を取得できるツールです。</p>
           <steps />
         </header>
