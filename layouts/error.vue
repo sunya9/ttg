d8ad1b4e6a2831745a8dc751662f36eadad0cf7c
sunya9 <mugen.xyz@gmail.com>
2017-09-27 22:44:38 +0900

    Fix error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,31 +1,17 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
+  <div class="container">
+    <h1>{{error.statusCode}}</h1>
+    <p>{{error.message}}</p>
     <nuxt-link to="/" class="button button-outline">Home</nuxt-link>
   </div>
 </template>
 
 <script>
-const CLIENT_ERROR = 4
-const SERVER_ERROR = 5
 export default {
-  propr: ['error'],
-  computed: {
-    title() {
-      const statusCode = !this.error ? 400 : this.error
-      const errorType = Math.floor(statusCode / 100)
-      const clientError = errorType === CLIENT_ERROR
-      const serverError = errorType === SERVER_ERROR
-      switch(statusCode) {
-        case 401:
-          return 'Unauthorized'
-        case 404:
-        default:
-          return serverError
-            ? 'Server error'
-            : 'Not found'
-
-      }
+  props: ['error'],
+  head() {
+    return {
+      title: `${this.error.statusCode}: ${this.error.message}`
     }
   }
 }
